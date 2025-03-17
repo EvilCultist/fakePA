7d1a1ce8506b5bc4f926f14a0a34aa0c6b5678f5
me <66961093+EvilCultist@users.noreply.github.com>
2025-03-17 21:11:19 +0530

    new server files

--- fakepa-server/src/main.zig
+++ fakepa-server/src/main.zig
@@ -15,14 +15,27 @@ const tn = struct {
 
 const Role = enum { user, admin, doctor };
 
-const form_body = struct {
+const chat_mssg = struct {
     role: Role,
     content: []const u8,
 };
 
+const new_user = struct {
+    name: []const u8,
+    psswd: []const u8,
+    dob: []const u8,
+    age: u8,
+    gender: []const u8,
+    height: u8,
+    weight: u8,
+    habits: []const u8,
+    medicalHistory: []const u8,
+    allergies: []const u8,
+};
+
 const n_tokens = 0;
 
-///json.static.Parsed(main.form_body){
+///json.static.Parsed(main.chat_mssg){
 ///     .arena = heap.arena_allocator.ArenaAllocator{
 ///         .child_allocator = mem.Allocator{
 ///             .ptr = anyopaque@0,
@@ -33,7 +46,7 @@ const n_tokens = 0;
 ///             .end_index = 0
 ///         }
 ///     },
-///     .value = main.form_body{
+///     .value = main.chat_mssg{
 ///         .role = main.Role.user,
 ///         .content = { 110, 111 }
 ///     }
@@ -44,8 +57,10 @@ const n_tokens = 0;
 //         std.process.exit(1);
 // }
 
+var pool: *pg.Pool = undefined;
+
 pub fn main() !void {
-    var pool = pg.Pool.init(stdalloc, .{ .size = 20, .connect = .{
+    pool = pg.Pool.init(stdalloc, .{ .size = 2, .connect = .{
         .port = 5432,
         .host = "127.0.0.1",
     }, .auth = .{
@@ -102,7 +117,7 @@ fn on_request(r: zap.Request) void {
     if (r.body) |the_body| {
         std.debug.print("BODY: {s}\n", .{the_body});
         if (std.json.parseFromSlice(
-            form_body,
+            chat_mssg,
             std.heap.page_allocator,
             the_body,
             .{},
@@ -125,8 +140,66 @@ fn on_request(r: zap.Request) void {
                 std.process.exit(1);
             }
         } else if (std.mem.eql(u8, the_path, "api/addUser")) {
-            if (r.sendBody("")) {
-                std.debug.print("added user\n", .{});
+            if (r.body) |the_body| {
+                std.debug.print("BODY: {s}\n", .{the_body});
+                if (std.json.parseFromSlice(
+                    new_user,
+                    std.heap.page_allocator,
+                    the_body,
+                    .{},
+                )) |parsed| {
+                    const val = parsed.value;
+                    _ = pool.query(
+                        \\ INSERT INTO public.patients (
+                        \\     medical_record_no,
+                        \\     name,
+                        \\     date_of_birth,
+                        \\     age,
+                        \\     gender,
+                        \\     weight,
+                        \\     height,
+                        \\     habits,
+                        \\     medical_history,
+                        \\     allergies,
+                        \\     password
+                        \\ )
+                        \\ VALUES (
+                        \\     uuid_generate_v4(),  -- Automatically generates a UUID
+                        \\     $1  -- 'John Doe',          f the patient
+                        \\     $2  -- '1990-05-15',        f birth
+                        \\     $3  -- 35,                  f the patient
+                        \\     $4  -- 'Male',              
+                        \\     $5  -- 75.5,                 (in kilograms)
+                        \\     $6  -- 175.0,                (in centimeters)
+                        \\     $7  -- 'Non-smoker',        
+                        \\     $8  -- 'No significant history',
+                        \\     $9  -- 'Peanuts',        
+                        \\     $10 -- 'hashed_password_123'  
+                        \\ );
+                    , .{
+                        val.name,
+                        val.psswd,
+                        val.dob,
+                        val.age,
+                        val.gender,
+                        val.height,
+                        val.weight,
+                        val.habits,
+                        val.medicalHistory,
+                        val.allergies,
+                    }) catch |err| {
+                        std.debug.print("query error {any}", .{err});
+                        std.process.exit(1);
+                    };
+
+                    std.debug.print("added user : {s}\n", .{parsed.value.name});
+                } else |err| {
+                    std.debug.print("could not parse user :( {any}\n", .{err});
+                }
+                return;
+            }
+            if (r.sendBody("user added")) {
+                // std.debug.print("added user\n", .{});
                 return;
             } else |err| {
                 std.debug.print("well see sometimes things happen\n {any}\n", .{err});
@@ -158,6 +231,14 @@ fn on_request(r: zap.Request) void {
             }
         } else if (std.mem.eql(u8, the_path, "/vocab.txt")) {
             if (r.sendFile("web/vocab.txt")) {
+                std.debug.print("It worked?\n", .{});
+                return;
+            } else |err| {
+                std.log.err("oh nards, some error happened\n{any}\n", .{err});
+                std.process.exit(1);
+            }
+        } else if (std.mem.eql(u8, the_path, "/words.txt")) {
+            if (r.sendFile("web/words.txt")) {
                 std.debug.print("It worked?\n", .{});
                 return;
             } else |err| {
